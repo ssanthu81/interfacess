e40604c2b640e1cc8ac93fca7e0c2b0164787c2a
ssanthu81 <101133033+ssanthu81@users.noreply.github.com>
2022-05-13 09:06:45 -0500

    Add files via upload

---
+++ interfacess/interfacess/Program.cs
@@ -0,0 +1,28 @@
+﻿
+interface test
+{
+    void add(int a, int b);
+}
+interface test2: test
+{
+    void sub(int a, int b);
+}
+class imp : test
+{
+    public void add(int a, int b)
+    {
+        Console.WriteLine(a + b);
+    }
+    public void sub(int a , int b)
+    {
+        Console.WriteLine(a - b);
+    }
+    static void Main()
+    {
+        imp ob = new imp();
+        ob.add(11, 33);
+        ob.sub(22, 33);
+        Console.ReadLine();
+    }
+
+}
